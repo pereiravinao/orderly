150b84dccc095f2dce3bfadda65d324efdac4f7e
Vinicius Norberto <vpereira.norberto@gmail.com>
2025-07-30 17:32:13 -0300

    Ajusta criacao da fila

--- apps/order-receiver-producer/src/main/java/challenge/tech/config/RabbitConfig.java
+++ apps/order-receiver-producer/src/main/java/challenge/tech/config/RabbitConfig.java
@@ -2,6 +2,8 @@ package challenge.tech.config;
 
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,5 +17,10 @@ public class RabbitConfig {
     @Bean
     public Queue orderReceiverQueue() {
         return QueueBuilder.durable(orderReceiverQueueName).build();
+    }
+
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
     }
 }
