35d82ebf5a2853127643baac1434fb42e3850e59
Vinicius Norberto <vpereira.norberto@gmail.com>
2025-07-30 17:48:29 -0300

    Ajusta criacao da fila para o order-service

--- apps/order-service/src/main/java/challenge/tech/config/RabbitConfig.java
+++ apps/order-service/src/main/java/challenge/tech/config/RabbitConfig.java
@@ -4,23 +4,21 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitConfig {
 
-    @Value("${application.queue.order-receiver}")
-    private String orderReceiverQueueName;
-
     @Bean
     public Queue orderReceiverQueue() {
-        return QueueBuilder.durable(orderReceiverQueueName).build();
+        return QueueBuilder.durable("order-receiver-queue").build();
     }
 
     @Bean
     public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
-        return new RabbitAdmin(connectionFactory);
+        RabbitAdmin admin = new RabbitAdmin(connectionFactory);
+        admin.setAutoStartup(true);
+        return admin;
     }
 }
